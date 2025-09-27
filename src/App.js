4c9f9d8fefa72a685bd17546b1f60ab6bea140af
36Ruch <pruchita814@gmail.com>
2025-09-27 16:48:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+  
 import Events from "./Pages/Events";
 import BuddyListing from "./Pages/BuddyListing";
 import PostRequest from "./Pages/PostRequest";
@@ -8,7 +9,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Events />} />
+         <Route path="/" element={<Events />} /> 
         <Route path="/buddies" element={<BuddyListing />} />
         <Route path="/post" element={<PostRequest />} />
       </Routes>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,39 +4,28 @@ import './Navbar.css';
 
 const Navbar = () => (
   <nav className="navbar">
-    <div className="navbar-links-container">
-      <ul className="navbar-links">
-        <li>
-          <NavLink to="/" end className={({ isActive }) => isActive ? 'active' : ''}>
-            Events
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/buddies" className={({ isActive }) => isActive ? 'active' : ''}>
-            Buddy Listing
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/post" className={({ isActive }) => isActive ? 'active' : ''}>
-            Post Request
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" className={({ isActive }) => isActive ? 'active' : ''}>
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup" className={({ isActive }) => isActive ? 'active' : ''}>
-            Sign Up
-          </NavLink>
-        </li>
-      </ul>
+    <div className="navbar-left">
+      {}
+    </div>
+
+    <ul className="navbar-links">
+      <li>
+        <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/events" className={({ isActive }) => isActive ? 'active' : ''}>Events</NavLink>
+      </li>
+      <li>
+        <NavLink to="/buddies" className={({ isActive }) => isActive ? 'active' : ''}>Buddy Listing</NavLink>
+      </li>
+      <li>
+        <NavLink to="/post" className={({ isActive }) => isActive ? 'active' : ''}>Post Request</NavLink>
+      </li>
+    </ul>
+
+    <div className="navbar-right">
+      <NavLink to="/login" className="login-btn">Login</NavLink>
+      <NavLink to="/signup" className="signup-btn">Signup</NavLink>
     </div>
   </nav>
 );
