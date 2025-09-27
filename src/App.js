136414e99ee6c696337cfb49cf9153d5e925a83c
36Ruch <pruchita814@gmail.com>
2025-09-27 09:28:39 +0530

    code in app.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import BuddyListing from "./Pages/BuddyListing";
+import PostRequest from "./Pages/PostRequest";
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buddies" element={<BuddyListing />} />
+        <Route path="/post" element={<PostRequest />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
 
