808a91b77f8b2edf1a666b7fd04cb064c03f0220
36Ruch <pruchita814@gmail.com>
2025-09-27 10:42:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import BuddyListing from "./Pages/BuddyListing";
-import PostRequest from "./Pages/PostRequest";
+
 
 
 function App() {
@@ -12,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/buddies" element={<BuddyListing />} />
-        <Route path="/post" element={<PostRequest />} />
       </Routes>
     </Router>
   );
