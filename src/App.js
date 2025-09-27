a1a1a46acce0e29549b5ea0e78c31dc15cc590f1
36Ruch <pruchita814@gmail.com>
2025-09-27 16:51:59 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-  
 import Events from "./Pages/Events";
 import BuddyListing from "./Pages/BuddyListing";
 import PostRequest from "./Pages/PostRequest";
